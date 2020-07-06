5069b90cdfef890e2d9ca29ad5126ac4b52da04b
角田道映 <michiaki.1267white@icloud.com>
2020-07-07 07:08:19 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -3,7 +3,16 @@
 int n;
 int k;
 int A[100000];
+int B[1000000000];
 
+int p(int m){
+    int l = 0;
+    int z;
+    for(z = 0;z < n;z++){
+        l = l + A[z] / B[m];
+    }
+    return l >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +20,21 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  int y;
+  for(y = 0;y <= A[n - 1];y++){
+      B[y] = A[n - 1] - y;
+  }
+  lb = -1;
+  ub = y - 1;
+  while(ub - lb > 1){
+      int mid = (lb + ub) / 2;
+      if(p(mid)){
+          ub = mid;
+      }
+      else{
+          lb = mid;
+      }
+  }
+  printf("%d\n",B[ub]);
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -3,7 +3,27 @@
 int n;
 int k;
 int A[100000];
+int B[1000000000];
 
+int p(int m){
+    int kk = 1;
+    int sigoto = 0;
+    int y;
+    for(y = 0;y < n;y++){
+        if(A[y] > m) return 0;
+    }
+    for(y = 0;y < n;y++){
+        if(sigoto <= m){
+            sigoto = sigoto + A[y];
+            if(sigoto > m){
+                sigoto = A[y];
+                kk++;
+            }
+        }
+    }
+    
+    return kk <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +31,28 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+    int sum = 0;
+    int x;
+    for(x = 0;x < n;x++){
+        sum = sum + A[x];
+    }
+    int z;
+    for(z = 0;z <= sum;z++){
+        B[z] = z;
+    }
+    
+    lb = -1;
+    ub = sum;
+    while(ub - lb > 1){
+        int mid = (lb + ub) / 2;
+        if(p(mid)){
+            ub = mid;
+        }
+        else{
+            lb = mid;
+        }
+    }
+    printf("%d\n",B[ub]);
 
   return 0;
 }
