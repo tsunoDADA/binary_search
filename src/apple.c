0d567fd7e46b3842e7384a9daba23623ced60d10
角田道映 <michiaki.1267white@icloud.com>
2020-07-07 07:06:28 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,17 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int l = 0;
+    int z;
+    for(z = 0;z < n;z++){
+        l = l + 1 + A[z] / A[m];
+        if(A[z]%A[m] == 0){
+            l = l - 1;
+        }
+    }
+    return l <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +22,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    lb = -1;
+    ub = n;
+    while(ub - lb > 1){
+        int mid = (lb + ub) / 2;
+        if(p(mid)){
+            ub = mid;
+        }
+        else{
+            lb = mid;
+        }
+    }
+    printf("%d\n",A[ub]);
   return 0;
 }
